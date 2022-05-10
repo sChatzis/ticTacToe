2cf3ec9be8d9b4f35f59313c83b1f604d2f62a60
stefanoschatzis <stevenchatzis66@gmail.com>
2022-05-10 17:07:28 +0300

    AYTOOOO

--- ticTacToe  --Chatzis-Michelakos/src/Model/Player.java
+++ ticTacToe  --Chatzis-Michelakos/src/Model/Player.java
@@ -1,5 +1,9 @@
 package Model;
 
 public class Player {
-
+	private String playerName;
+	private int games, wins, loses;
+	private GameRecord[] history_score;
+	private GameRecord[] best_games; 
+	
 }
